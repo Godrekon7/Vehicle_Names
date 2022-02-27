c7e78be1148ce1841062764dba6baf15b7618a34
Godrekon7 <100534811+Godrekon7@users.noreply.github.com>
2022-02-27 21:16:28 +0100

    Add files via upload

---
+++ _resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+games { 'gta5' }
+
+author 'Godrekon7#2242 Discord'
+description 'Addon Vehicles Name Reader'
+version '1.0.0'
+
+client_scripts {
+    'vehicle_names.lua'
+}
---
+++ vehicle_names.lua
@@ -0,0 +1,4 @@
+Citizen.CreateThread(function()
+    AddTextEntry('gameName', 'Desidered Text')
+    --AddTextEntry('gameName', 'desidered text')
+end)
